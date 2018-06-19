a6945f2303897724e3d70c7dfa6c6bc371b363d9
David Beck <code@davidbeck.co>
2018-06-18 21:27:39 -0700

    Change cli name

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "generate_uiimages",
+            name: "vectoriogen",
             dependencies: [
 				"VectorIO",
 			]
